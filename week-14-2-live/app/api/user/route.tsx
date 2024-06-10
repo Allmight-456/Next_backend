ddb0cc520e969932e0c67f646e5256e8183d3b29
Ishan Kumar <ishankumar@Ishans-MacBook-Air.local>
2024-06-10 07:35:35 +0530

    added the database fetching to the frontend

--- week-14-2-live/app/api/user/route.tsx
+++ week-14-2-live/app/api/user/route.tsx
@@ -4,23 +4,34 @@ import { PrismaClient } from "@prisma/client";
 const client = new PrismaClient();
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  // Database logic
-  await client.user.create({
-    data: {
-      username: body.username,
-      password: body.password,
-    },
-  });
+  try {
+    const body = await req.json();
+    // Database logic
+    await client.user.create({
+      data: {
+        username: body.username,
+        password: body.password,
+      },
+    });
 
-  return NextResponse.json({
-    msg: "You are logged in"
-  });
+    return NextResponse.json({
+      msg: "You are logged in"
+    });
+  } catch (error) {
+    console.error("Error during user creation/user already exists", error);
+
+    // Assuming that an error indicates a problem with the input, like a wrong password
+    return NextResponse.json({
+      error: "User already exists , Sign In"
+    }, { status: 400 });
+  }
 }
 
-export async function GET() {
+//now for fetching the data from the database and displaying it
+export async function GET(req:NextRequest) {
+  const user = await client.user.findFirst()
   return  NextResponse.json({
-    name: "Ishan Kumar",
+    name: user?.username,
     email: "bhardwajishansingh@gmail.com",
   });
 }
